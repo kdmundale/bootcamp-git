bdfa40cff08c3ab916d30c01f3510cbd5d5a3ca1
Katherine Mundale <katherine.doris.mundale@gmail.com>
2019-09-17 12:54:36 -0400

    Revert "Revert "resolved merge conflict""
    This reverts commit 0599b65b3e46819b0f41f078c37ca4104d50f350.

--- dog.c
+++ dog.c
@@ -4,6 +4,8 @@ canine
 wolf
 bark
 bow wow
+howl
+<<<<<<< HEAD
 pikachu
 charmander
 squirtle
@@ -14,3 +16,4 @@ ivysaur
 venasaur
 wartortle
 blastoise
+>>>>>>> fbb1dd1b61f4c2771d66f0a73626efce43f7dea6
